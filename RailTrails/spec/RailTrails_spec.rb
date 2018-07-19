8d5040108c97c6a1673883db2a7b17531fce7b78
Valerie Moore <periwinkle17@outlook.com>
2018-07-19 14:25:05 +0000

    cloned RailTrails repo

--- RailTrails/spec/RailTrails_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe RailTrails do
-  it "has a version number" do
-    expect(RailTrails::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
